62e4f535fd58e3cf6824634c3a9d8e281623f763
Shamiul-Lipu <shamiullipu@gmail.com>
2024-02-19 05:48:08 +0600

    react router tutorial doc done

--- React Router v6/react-router-official-tutorial/src/main.jsx
+++ React Router v6/react-router-official-tutorial/src/main.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./Root.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import Contact from "./Contact.jsx";
+import {
+  getContactLoader,
+  getContactsLoader,
+} from "./loaders/contactsLoader.jsx";
+import {
+  createContactAction,
+  deleteContactAction,
+  editContactAction,
+  updateContactFavoriteAction,
+} from "./actions/contactsActions.js";
+import EditContact from "./EditContact.jsx";
+import Index from "./index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "/contacts/:contactId",
+            element: <Contact />,
+            loader: getContactLoader,
+            action: updateContactFavoriteAction,
+          },
+          {
+            path: "/contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: getContactLoader,
+            action: editContactAction,
+          },
+          {
+            path: "/contacts/:contactId/destroy",
+            action: deleteContactAction,
+            errorElement: <div>This was an deleting error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
